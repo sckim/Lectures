59c5132f97bf0a34e674e07d6a8f2abdc5127ab6
Soochan Kim <dr.soochankim@gmail.com>
2024-09-25 12:48:33 +0900

    Add asmStart & asmBlink examples

---
+++ MicrochipStudio/asmBlink/main.asm
@@ -0,0 +1,16 @@
+;
+; asmBlink.asm
+;
+; Created: 2024-09-25 오전 2:12:45
+; Author : Soochan Kim
+;
+
+
+; Replace with your application code
+Setup:
+    	sbi DDRB,DDB5 ; pinMode(PB5, HIGH)
+ 
+Loop:
+    	cbi PORTB,PB5 ; digitalWrite(PB5, LOW) 
+    	sbi PORTB,PB5 ; digitalWrite(PB5, HIGH) 
+    	rjmp Loop ; Jump relative back to label Loop, 2 clock cycles
---
+++ MicrochipStudio/asmStart/main.asm
@@ -0,0 +1,12 @@
+;
+; asmStart.asm
+;
+; Created: 2024-09-25 오전 2:12:16
+; Author : Soochan Kim
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
